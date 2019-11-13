1a3cb7709da64a83bad69234b3087748ec1ed4a6
Ngoding Terus <ngodingterus81@gmail.com>
2019-11-13 14:12:36 +0700

    mengubah gambar banner

--- application/views/banner.php
+++ application/views/banner.php
@@ -1,19 +1,30 @@
+<style>
+    img{
+       filter:alpha(opacity=60);
+       -moz-opacity:.6;
+       opacity:.6;
+    }
+    .shadow-text{
+        text-shadow:4px 3px 5px rgba(0,0,0,.7);
+    }
+</style>
+
 <a id="home"></a>
 <!-- ========== BANNER START ========== -->
 <div id="slides">
-    <div class="slides-container">
-        <img src="<?=base_url('assets/')?>images/banner1.jpg" alt="" />
-        <img src="<?=base_url('assets/')?>images/banner2.jpg" alt="" />
-        <img src="<?=base_url('assets/')?>images/banner3.jpg" alt="" />
-        <img src="<?=base_url('assets/')?>images/banner4.jpg" alt="" />
-        <img src="<?=base_url('assets/')?>images/banner5.jpg" alt="" />
+    <div class="slides-container" style="background-color:black;">
+        <img height="500" src="<?=base_url('assets/')?>images/banner1.webp" alt="..." />
+        <img height="500" src="<?=base_url('assets/')?>images/banner2.webp" alt="..." />
+        <img height="500" src="<?=base_url('assets/')?>images/banner3.webp" alt="..." /> -->
+        <!-- <img height="500" src="<?=base_url('assets/')?>images/banner4.jpg" alt="..." />
+        <img height="500" src="<?=base_url('assets/')?>images/banner5.jpg" alt="..." /> -->
     </div>
     <div class="tint">
         <div class="welcome text-center" data-scroll-reveal="enter from the top after 2s">
-            <h1>HomestayQu.com</h1>
+            <h1 class="shadow-text">HomestayQu.com</h1>
             <br>
-            <p>|<span></span><i class="fa fa-home"></i><span></span>|</p>
-            <p><b>12 September 1999</b></p>
+            <p class="shadow-text">|<span></span><i class="fa fa-home"></i><span></span>|</p>
+            <p class="shadow-text"><b>12 September 1999</b></p>
         </div>
     </div>
     <div class="countdown" data-scroll-reveal="enter from the bottom after 2.5s"></div>
